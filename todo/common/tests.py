0ab5c86634f06c94b3900cb2973e1634996a141d
Stanislav Buldyhin <stanislav.buldyhin@geo-pay.net>
2023-04-09 18:11:58 +0300

    update Jenkinsfile

--- todo/common/tests.py
+++ todo/common/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Task
+
+
+class CommonTestCase(TestCase):
+
+    def test_successful_adding_tasks(self):
+        Task.objects.create(title='test')
+        task = Task.objects.filter().first()
+        self.assertEqual(task.title, 'test')
